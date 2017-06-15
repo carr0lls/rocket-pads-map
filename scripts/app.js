83cf0e2d055afbd385c9e973347fe564d25e9ee3
Carroll Yu <carrollcyu@gmail.com>
2017-06-14 22:56:45 -0700

    init commit

---
+++ scripts/app.js
@@ -0,0 +1,199 @@
+var initialize = (function() {
+
+	var CONFIG = {
+		'DEBUG': false // Enable to use dummy data if API server is down
+	}
+	var API = {
+		'ROCKET_PADS': 'https://launchlibrary.net/1.2/pad?mode=verbose',
+		'ROCKETS': 'https://launchlibrary.net/1.2/rocket?mode=verbose'
+	}
+	var ROCKETPADS = {};
+	var ROCKETS = {};
+	var MAP;
+
+	function fetchRocketPads(offset = 0) {
+		return function(resolve, reject) {
+			if (CONFIG.DEBUG)
+				return resolve(SEED.dummy_pads);
+
+			fetch(API.ROCKET_PADS + '&offset=' + offset)
+				.then(function(response) {
+					return response.json();
+				})
+				.then(function(data) {
+					return resolve(data);
+				}).
+				catch(function(error) {
+					var errorObject = { 
+						reason: 'failed to retrieve rocket pads data',
+						error: error
+					}
+					return reject(errorObject);
+				});
+		}
+	}
+
+	function fetchRockets(offset = 0) {
+		return function(resolve, reject) {
+			if (CONFIG.DEBUG)
+				return resolve(SEED.dummy_rockets);
+
+			fetch(API.ROCKETS + '&offset=' + offset)
+				.then(function(response) {
+					return response.json();
+				})
+				.then(function(data) {
+					return resolve(data);
+				}).
+				catch(function(error) {
+					var errorObject = { 
+						reason: 'failed to retrieve rockets data',
+						error: error
+					}
+					return reject(errorObject);
+				});
+		}
+	}
+
+	function initialFetch() {
+		var promises = [];
+		promises.push(new Promise(fetchRocketPads()));
+		promises.push(new Promise(fetchRockets()));
+
+		return Promise.all(promises);
+	}
+
+	// Fetch per page because API Rocket and Pad endpoints don't allow limits to be set off
+	function fullFetch(data) {
+		var rocketPadPromises = [], rocketPromises = [], combinedPromises = [];
+		var rocketPadsData = data[0], rocketsData = data[1];
+		var offset = 0; 
+
+		while (offset < parseInt(rocketPadsData['total'])) {
+			rocketPadPromises.push(new Promise(fetchRocketPads(offset)));
+			offset += parseInt(rocketPadsData['count']);
+		}
+
+		offset = 0;
+		while (offset < parseInt(rocketsData['total'])) {
+			rocketPromises.push(new Promise(fetchRockets(offset)));
+			offset += parseInt(rocketsData['count']);
+		}
+
+		combinedPromises.push(Promise.all(rocketPadPromises));
+		combinedPromises.push(Promise.all(rocketPromises));
+
+		return Promise.all(combinedPromises);
+	}
+
+	function mergeData(data) {
+		var rocketPadsData = data[0], rocketsData = data[1];
+		var padIds;
+
+		// Build rocket pads look-up table
+		rocketPadsData.map(function(data) {
+			for (var rocketPad of data['pads']) {
+				ROCKETPADS[rocketPad['id']] = rocketPad;
+			}
+		});
+		// Build rockets look-up table
+		rocketsData.map(function(data) {
+			for (var rocket of data['rockets']) {
+				ROCKETS[rocket['id']] = rocket;
+				// Attach rocket id's to rocket pads look-up table
+				if (rocket['defaultPads']) {
+					padIds = rocket['defaultPads'].split(',');
+
+					for (var pid of padIds) {
+						if (ROCKETPADS[pid]) {
+							if (ROCKETPADS[pid]['rockets'] === undefined)
+								ROCKETPADS[pid]['rockets'] = [rocket['id']];
+							else
+								ROCKETPADS[pid]['rockets'].push(rocket['id']);
+						}
+					}
+				}
+			}
+		});
+	}
+
+	function addMarkerToMap(padInfo, rocketInfo=false) {
+		var marker, infoWindow, myLatlng, contentString = '';
+
+		marker = new google.maps.Marker({
+			map: MAP,
+		    position: new google.maps.LatLng(padInfo['latitude'], padInfo['longitude']),
+		    name: padInfo['name'],
+		    rockets: rocketInfo
+		});
+
+		Object.entries(rocketInfo).forEach(function([key, rocket]) {
+			if (rocket['imageURL']) {
+				contentString += `
+					<div>
+						<a href="${rocket['wikiURL']}" target="_blank">
+							<label>${rocket['name']}</label>
+							<div><img src="${rocket['imageURL']}" alt="${rocket['name']}" width="40px" /></div>
+						</a>
+					</div>`;
+			}
+			else {
+				contentString += `
+				<div>
+					<a href="${rocket['wikiURL']}" target="_blank">${rocket['name']}</a>
+				</div>`;
+			}
+		});
+
+		infoWindow = new google.maps.InfoWindow({
+			content: `
+				<div>
+					<a href="${padInfo['wikiURL']}" target="_blank"><h2>${padInfo['name']}</h2></a>
+					${contentString}
+				</div>`
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infoWindow.open(MAP, marker);
+		});		
+	}
+
+	function renderDataToMap() {
+		var rocketInfo;
+		
+		Object.entries(ROCKETPADS).forEach(function([key, rpad]) {
+			rocketInfo = {};
+			if (rpad['rockets']) {
+				for (var rid of rpad['rockets']) {
+					rocketInfo[rid] = { 
+						name: ROCKETS[rid]['name'], 
+						imageURL: ROCKETS[rid]['imageURL'],
+						wikiURL: ROCKETS[rid]['wikiURL']
+					};
+				}
+			}
+			addMarkerToMap(rpad, rocketInfo);
+		});
+	}
+
+	function renderMap() {
+		var mapOptions = {
+		  zoom: 2,
+		  center: new google.maps.LatLng(25.6579032, -8.1180046)
+		}
+		MAP = new google.maps.Map(document.getElementById("map"), mapOptions);
+	}
+
+	function init() {
+		renderMap();
+
+		initialFetch()
+			.then(fullFetch)
+			.then(mergeData)
+			.then(renderDataToMap)
+			.catch(function(error) { console.log(error); });
+	}
+
+	return init;
+
+})();
---
+++ scripts/seed.js
@@ -0,0 +1,29 @@
+var SEED = {
+	dummy_rockets: {
+		offset: 0, 
+		count: 30, 
+		total: 10,
+		rockets: [
+			{id: 1, name: 'rocket1', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Minotaur_IV', defaultPads: '1,2,3'},
+			{id: 2, name: 'rocket2', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Soyuz-2', defaultPads: '3,4,5'},
+			{id: 3, name: 'rocket3', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Minotaur_IV', defaultPads: '1,'},
+			{id: 4, name: 'rocket4', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Soyuz-2', defaultPads: '2'},
+			{id: 5, name: 'rocket5', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Soyuz-2', defaultPads: '3'},
+			{id: 6, name: 'rocket6', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Minotaur_IV', defaultPads: '4,5'},
+			{id: 7, name: 'rocket7', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Soyuz-2', defaultPads: '2,3'},
+			{id: 8, name: 'rocket8', imageURL: 'https://s3.amazonaws.com/launchlibrary/RocketImages/H-IIA_320.jpg', wikiURL: 'https://en.wikipedia.org/wiki/Minotaur_IV', defaultPads: '5,3'}
+		]
+	},
+	dummy_pads: {
+		offset: 0, 
+		count: 30, 
+		total: 10, 
+		pads: [
+			{id: 1, name: 'pad1', imageURL: '', wikiURL: 'https://en.wikipedia.org/wiki/Gran_Canaria', latitude: 25.6579032, longitude: -8.1180046},
+			{id: 2, name: 'pad2', imageURL: '', wikiURL: 'https://en.wikipedia.org/wiki/Gran_Canaria', latitude: -25.6579032, longitude: 8.1180046},
+			{id: 3, name: 'pad3', imageURL: '', wikiURL: 'https://en.wikipedia.org/wiki/Gran_Canaria', latitude: 20.6579032, longitude: -4.1180046},
+			{id: 4, name: 'pad4', imageURL: '', wikiURL: 'https://en.wikipedia.org/wiki/Gran_Canaria', latitude: -20.6579032, longitude: 4.1180046},
+			{id: 5, name: 'pad5', imageURL: '', wikiURL: 'https://en.wikipedia.org/wiki/Gran_Canaria', latitude: 5.6579032, longitude: -8.1180046}
+		]
+	}
+};
